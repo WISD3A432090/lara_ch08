7460818b131568c5244b9dbe904cf2e9f2e44e1e
M511-50\admin <s3a432090@student.ncut.edu.tw>
2017-10-16 09:29:25 +0800

    正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."}成績";});
 */
+Route::get('student/{student_no}',function ($student_no){
+    return "學號：".$student_no;
+})->where(['student_no'=>'s[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null) {
+    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."}成績";
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese | english | math)']);
 
-Route::get('syudent/{student_no}/score/{subject?}',function($student_no,$subject=null){
-    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";});
-
+Route::pattern('student_no','s[0-9]{10}');
+Route::get('student/{student_no}',function ($student_no) {
+    return "學號：" . $student_no;
+});
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null) {
+    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."}成績";
+})->where(['subject'=>'(chinese | english | math)']);

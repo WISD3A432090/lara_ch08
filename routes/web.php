be7d001384fa251fe557d6f59d570236a1d4e266
M511-50\admin <s3a432090@student.ncut.edu.tw>
2017-10-16 10:06:49 +0800

    建立Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/* ROUTE 5-4~5-7
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."}成績";});
 
@@ -29,7 +29,7 @@ Route::group(['prefix'=>'student'],function() {
         return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "}成績";
     })->where(['student_no' => 's[0-9]{10}', 'subject' => '(chinese | english | math)']);
 });
-*/
+
 Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'],function() {
     Route::get('{student_no}',[
@@ -45,3 +45,5 @@ Route::group(['prefix'=>'student'],function() {
         }
     ])->where([ 'subject' => '(chinese | english | math)']);
 });
+*/
+

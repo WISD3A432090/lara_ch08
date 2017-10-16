6c2cab1c685d7ab0de8576571e85c4dce735e951
M511-50\admin <s3a432090@student.ncut.edu.tw>
2017-10-16 09:55:14 +0800

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('student/{student_no}',function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null) {
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."}成績";
 })->where(['subject'=>'(chinese | english | math)']);
-*/
+
+
 Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'],function() {
     Route::get('{student_no}', function ($student_no) {
@@ -27,4 +28,20 @@ Route::group(['prefix'=>'student'],function() {
     Route::get('student/{student_no}/score/{subject?}', function ($student_no, $subject = null) {
         return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "}成績";
     })->where(['student_no' => 's[0-9]{10}', 'subject' => '(chinese | english | math)']);
+});
+*/
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'],function() {
+    Route::get('{student_no}',[
+        'as'=>'student',
+        'use'=>function ($student_no) {
+            return '學號：' . $student_no;
+        }
+    ]);
+    Route::get('student/{student_no}/score/{subject?}', [
+        'as'=>'student.score',
+        'uses'=>function ($student_no, $subject = null) {
+            return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '}成績';
+        }
+    ])->where([ 'subject' => '(chinese | english | math)']);
 });

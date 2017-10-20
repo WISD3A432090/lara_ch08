f3478b85a64ffb8f15040e4b82cc72d84bd4bd24
MicoDER電腦\MICO <s3a432090@student.ncut.edu.tw>
2017-10-20 23:20:09 +0800

    讓視圖顯示動態資料

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Http\Requests;
+use App\Student;
+use view;
 
 class StudentController extends Controller
 {
-    public function getStudentData($student_no){
-        return "學號：" . $student_no;
-    }
-    public function getStudentScore($student_no, $subject = null)
+    public function getStudentData($student_no)
     {
-        return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "}成績";
+        return View::make('score', [
+            'student_no' => $student_no,
+            'subject' => null
+        ]);
     }
 
+
+    public function getStudentScore($student_no, $subject = null)
+    {
+        return View::make('score', [
+            'student_no' => $student_no,
+            'subject' => $subject
+        ]);
+    }
 }
+       

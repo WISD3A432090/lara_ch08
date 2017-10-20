1d837e0ba3335802bf9be32afe84691ec21d1ce6
MicoDER電腦\MICO <s3a432090@student.ncut.edu.tw>
2017-10-20 22:56:47 +0800

    定義共用區塊

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,14 @@
+<div class="navber navber-default navber-fixed-top">
+    <div class="container">
+    <a class="navber-brand" href="{{ action('HomeController@index')}} ">HelloLaravel</a>
+    <ul class="nav navber-nav">
+        <li>
+        <a href="{{ action('BoardController@getIndex')  }}">排行榜 </a>
+        </li>>
+    </ul>
+</div>
+</nav>
+<div style="padding-top: 70px"></div>
+
+
+
